c4113efbf19855fb9765c78782932fef48a1ab4a
sruthy <sruthylekshmivijayan@gmail.com>
2024-04-21 00:27:09 +0530

    practice programs

---
+++ src/Lab003.java
@@ -0,0 +1,9 @@
+public class Lab003 {
+   // Q - int a = 10; System.out.println(++a + a++ + a++);
+
+    public static void main(String[] args)
+    {
+        int a =10;
+        System.out.println(++a + a++ + a++);
+    }
+}
---
+++ src/Lab004.java
@@ -0,0 +1,5 @@
+public class Lab004 {
+    public static void main(String[] args){
+    int b = 10;
+    System.out.println(--b + b++ + ++b);
+}}
